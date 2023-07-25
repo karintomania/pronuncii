97dcc003b552ebef58270d5d25b0784d105f5793
karintomania <19652340+karintomania@users.noreply.github.com>
2023-07-25 16:51:38 +0100

    working mvp (not really)

---
+++ src/main/static/main/test.js
@@ -0,0 +1,80 @@
+const record = document.querySelector('#record');
+const recordAgain = document.querySelector('#record-again');
+const stop = document.querySelector('#stop');
+const soundClips = document.querySelector('.sound-clips');
+const recordingFile = document.querySelector('#id_recording');
+
+//main block for doing the audio recording
+
+if (navigator.mediaDevices.getUserMedia) {
+  console.log('getUserMedia supported.');
+
+  const constraints = { audio: true };
+  let chunks = [];
+
+  let onSuccess = function(stream) {
+    const mediaRecorder = new MediaRecorder(stream);
+
+    record.onclick = function() {
+      mediaRecorder.start();
+      console.log(mediaRecorder.state);
+      console.log("recorder started");
+
+    }
+
+    stop.onclick = function() {
+      mediaRecorder.stop();
+      console.log(mediaRecorder.state);
+      console.log("recorder stopped");
+      // mediaRecorder.requestData();
+
+    }
+
+    mediaRecorder.onstop = function(e) {
+      console.log("data available after MediaRecorder.stop() called.");
+
+      const clipName = "your recording"
+
+      const clipContainer = document.createElement('article');
+      const audio = document.createElement('audio');
+
+      clipContainer.classList.add('clip');
+      audio.setAttribute('controls', '');
+
+      clipContainer.appendChild(audio);
+      soundClips.appendChild(clipContainer);
+
+      audio.controls = true;
+      const blob = new Blob(chunks, { 'type' : 'audio/ogg; codecs=opus' });
+      chunks = [];
+      const audioURL = window.URL.createObjectURL(blob);
+      audio.src = audioURL;
+
+      let container = new DataTransfer();
+      let file = new File([blob], "sound.wav",{type:"audio/wav"});
+      container.items.add(file);
+      recordingFile.files = container.files;
+
+    }
+
+    recordAgain.onclick = function(){
+        document.querySelector(".clip").remove();
+        record.click();
+    }
+
+    mediaRecorder.ondataavailable = function(e) {
+      chunks.push(e.data);
+    }
+  }
+
+  let onError = function(err) {
+    console.log('The following error occured: ' + err);
+  }
+
+  navigator.mediaDevices.getUserMedia(constraints).then(onSuccess, onError);
+
+} else {
+   console.log('getUserMedia not supported on your browser!');
+}
+
+
--- src/main/views.py
+++ src/main/views.py
@@ -6,6 +6,7 @@ from .services import test_service
 from django.core import serializers
 from pprint import pp
 from django.shortcuts import redirect
+from .forms import NameForm
 
 # Create your views here.
 def index(request):
@@ -35,23 +36,31 @@ def test(request):
     current_index = request.session[Config.SESSION_CURRENT_INDEX]
     sentence = request.session[Config.SESSION_SENTENCES][current_index]
 
+    form = NameForm()
+
     # show one question
     return render(request, 'main/test.html', {
         "sentence": sentence,
         "current_index": current_index,
+        "form": form,
     })
 
 def next(request):
 
-    # get sentence
-    current_index = request.session[Config.SESSION_CURRENT_INDEX]
-    sentence = request.session[Config.SESSION_SENTENCES][current_index]
+    print(len(request.FILES["recording"]))
+    form = NameForm(request.POST, request.FILES)
 
-    # update session
-    sentence["sound_path"] = "sound_path"
-    request.session[Config.SESSION_SENTENCES][request.session[Config.SESSION_CURRENT_INDEX]] = sentence
-    request.session[Config.SESSION_CURRENT_INDEX] += 1
-    request.session.modified = True
+    if form.is_valid():
+        handle_uploaded_file(request.FILES["recording"])
+        # get sentence
+        current_index = request.session[Config.SESSION_CURRENT_INDEX]
+        sentence = request.session[Config.SESSION_SENTENCES][current_index]
+
+        # update session
+        sentence["sound_path"] = form.cleaned_data['your_name']
+        request.session[Config.SESSION_SENTENCES][request.session[Config.SESSION_CURRENT_INDEX]] = sentence
+        request.session[Config.SESSION_CURRENT_INDEX] += 1
+        request.session.modified = True
 
     return redirect("main:test")
 
@@ -62,3 +71,8 @@ def result(request):
 class Config():
     SESSION_CURRENT_INDEX = "current_sentence_index"
     SESSION_SENTENCES = "sentences"
+
+def handle_uploaded_file(f):
+    with open("./sound.wav", "wb+") as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)

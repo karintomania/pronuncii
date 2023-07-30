133cb9eaf862d388dc36e05832d95c6ab8058933
karintomania <19652340+karintomania@users.noreply.github.com>
2023-07-30 16:05:49 +0100

    remove test text input

--- src/main/forms.py
+++ src/main/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 
 
-class NameForm(forms.Form):
-    your_name = forms.CharField(label="Your name", max_length=100)
+class FileForm(forms.Form):
     recording = forms.FileField()

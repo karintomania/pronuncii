bec403e9ff89db94fa0f9dbc5bb38147bf7972bd
karintomania <19652340+karintomania@users.noreply.github.com>
2023-08-17 18:46:49 +0100

    delete magic number from test

--- src/main/tests/test_models.py
+++ src/main/tests/test_models.py
@@ -6,30 +6,14 @@ from main.models import Sentence
 # Create your tests here.
 class SentenceTest(TestCase):
     def setUp(self) -> None:
-        Sentence.objects.create(
-            sentence="test 1",
-            pronunciation_sound_url="url1",
-        )
-        Sentence.objects.create(
-            sentence="test 2",
-            pronunciation_sound_url="url2",
-        )
-        Sentence.objects.create(
-            sentence="test 3",
-            pronunciation_sound_url="url3",
-        )
-        Sentence.objects.create(
-            sentence="test 4",
-            pronunciation_sound_url="url4",
-        )
-        Sentence.objects.create(
-            sentence="test 5",
-            pronunciation_sound_url="url5",
-        )
-        Sentence.objects.create(
-            sentence="test 6",
-            pronunciation_sound_url="url6",
-        )
+        sentence_count = Sentence.objects.TEST_SENTENCES_COUNT
+
+        for i in range(sentence_count + 1):
+            Sentence.objects.create(
+                sentence=f"test {i+1}",
+                pronunciation_sound_url=f"url{i+1}",
+            )
+
         return super().setUp()
 
     def test_get_sentence_for_test(self) -> None:
